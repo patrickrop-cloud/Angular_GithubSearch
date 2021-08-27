2d0381744c05b31fcefbd602eccb2be629f6bdb2
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-28 00:51:44 +0300

    get access github api key and gigt ignore environment.ts.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "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"
 };
 
 /*
