cde8b3a63b6a26df3fb7f2ccc172b9cf3b9c006f
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-28 04:30:26 +0300

    add stylyng to the app.

--- src/app/service/github-http-request.service.ts
+++ src/app/service/github-http-request.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import { User } from '.user';
-import { Repos } from './repos';
+import { User } from '../user';
+import { Repos } from '../repos';
 import { environment } from '../../environments/environment';
 
 @Injectable({
